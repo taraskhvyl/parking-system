11b127fa39850ae2687c954d721bc419c41f3816
Taras <taras.khvyl@gmail.com>
2023-08-20 23:14:42 +0200

    WIP: added Prisma

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,6 +1,5 @@
 import {Controller, Get} from '@nestjs/common';
 import { AppService } from './app.service';
-import {Logger} from "winston";
 
 @Controller()
 export class AppController {
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,16 +2,15 @@ import { Module, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WinstonModule } from 'nest-winston';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import {PrismaService} from "./prisma/prisma.service";
 
 @Module({
   imports: [
       ConfigModule.forRoot(),
       WinstonModule.forRoot({}),
-      MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.sawroob.mongodb.net/?retryWrites=true&w=majority`)
   ],
   controllers: [AppController],
-  providers: [Logger, AppService],
+  providers: [Logger, AppService, PrismaService],
 })
 export class AppModule {}
